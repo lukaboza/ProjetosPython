e2f33c7711717c1dcaa6775882b83db28682a3d0
Luka Amaral Boza <luka.boza@hotmail.com>
2021-08-15 20:48:16 -0300

    Att

--- Ex08.py
+++ Ex08.py
@@ -4,6 +4,6 @@ transforme e mostre a temperatura em graus Celsius.
 '''
 
 graus_f = int(input('Informe a temperatura em Fahrenheit: '))
-graus = 5* ((graus_f - 32)/ 9)
+graus = 5 * ((graus_f - 32)/ 9)
 
 print(f'{graus_f} Fahrenheit corresponde a {graus} graus.')
