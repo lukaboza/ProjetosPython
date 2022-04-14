91bc163ae3f7fe36b79039122f4f3f3db6bacacc
Luka Amaral Boza <luka.boza@hotmail.com>
2022-04-14 19:22:16 -0300

    Org-material

--- teste.py
+++ teste.py
@@ -1,6 +1,36 @@
-sexo = str(input('sexo: ')).upper()
+from flask import Flask , render_template, jsonify, request
 
-while sexo not in 'MmFf':
-    sexo = str(input('sexooooooo: '))
+app = Flask(__name__)
 
-print(sexo)
+@app.route('/')
+def principal():
+    return  'Ola'
+
+@app.route('/teste')
+def teste():
+    return  render_template("teste.html")
+
+@app.route('/testedoido/<string:nome>/<string:idade>')
+def testepriziada(nome, idade):
+    return  jsonify({'nome':nome, 'idade':idade})
+
+
+@app.route('/jogo', methods=["GET", "POST"])
+def jogo():
+    if request.method == "GET":
+        return render_template("jogo.html")
+    else:
+        numero = 0
+        palpite = int(request.form.get("name"))
+        if numero == palpite:
+            return 'Acertou'
+        else:
+            return 'Errouuuuuuu'
+
+
+@app.route('/<string:nome>')
+def error(nome):
+    variavel = f'pagina {nome} não tem'
+    return render_template("error.html", variavel_nome = variavel)
+
+app.run(debug=True)
